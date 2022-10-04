3f3c29cf69725f5f23b9ae151f4b947350b63203
LuisSoraca27 <Luisenriquesoraca27@gmail.com>
2022-10-03 22:09:20 -0500

    second commit

--- controllers/carts.controller.js
+++ controllers/carts.controller.js
@@ -1,15 +1,35 @@
-
-
 //models
-const { Cart } = require('../models/carts.model');
+const { ProductsInCart } = require("../models/productsInCart.model");
+
+// Utils
+const { catchAsync } = require("../utils/catchAsync.util");
+
+const addProductInCart = catchAsync(async (req, res, next) => {
+  const { cart } = req;
+  const { productId, quantity } = req.body;
+
+  const newProductInCart = await ProductsInCart.create({
+    cartId: cart.id,
+    productId,
+    quantity,
+  })
+  res.status(201).json({
+    status: 'success',
+    data: {newProductInCart}
+  })
+
+});
+
+const updateProductInCart = catchAsync( async (req, res, next) => {
+
+    const { ProductsInCart } = req
+    const { quantity } = req.body
+
+    ProductsInCart.update({ quantity })
+}) 
 
 
-const addProductInCart = () => {
-
-    const { sessionUser } = req
-
-    const { productId, quantity} = req.body
-
-    
-
+module.exports = {
+    addProductInCart,
+    updateProductInCart,
 }
--- routes/carts.routes.js
+++ routes/carts.routes.js
@@ -1,21 +1,39 @@
 const express = require("express");
 
 //controllers
-
+const { addProductInCart, updateProductInCart } = require("../controllers/carts.controller");
 
 //middlewares
-const {
-    protectSession,
-  } = require("../middlewares/auth.middlewares");
-const { cartExist, ProductExcess } = require("../middlewares/carts.middlewares")
+const { protectSession } = require("../middlewares/auth.middlewares");
 
+const {
+  cartExist,
+  ProductExcess,
+  productExistInCart,
+  searchOrCreateCart,
+  updateStatusProduct,
+} = require("../middlewares/carts.middlewares");
 
 const cartsRouter = express.Router();
 
-cartsRouter.post('/add-product', protectSession, cartExist, ProductExcess )
+cartsRouter.use(protectSession);
 
+cartsRouter.post(
+  "/add-product",
+  searchOrCreateCart,
+  ProductExcess,
+  productExistInCart,
+  addProductInCart
+);
 
+cartsRouter.patch(
+  "/update-cart",
+  cartExist,
+  ProductExcess,
+  updateStatusProduct,
+  updateProductInCart,
+);
 
 module.exports = {
-    cartsRouter,
-}
+  cartsRouter,
+};
